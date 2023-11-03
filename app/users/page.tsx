d269e514f35a3169b96889525102cd8fa245f341
kimnewbie <gracecancho@gmail.com>
2023-11-03 23:44:46 +0900

    css 적용해보기

---
+++ app/users/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    // axios를 사용하지 않을 경우에
+    cache: "no-store",
+    // next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleDateString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
